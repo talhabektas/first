64452f4810ecfc190ee0d8ae1e5e41bacc9055ac
talhabektas <115807895+talhabektas@users.noreply.github.com>
2022-10-14 19:20:09 +0300

    Initial commit

---
+++ src/first/classssss.java
@@ -0,0 +1,23 @@
+package first;
+
+public class classssss {
+
+	public static void main(String[] args) {
+		int[] sayilar = new int[] { 1, 2, 5, 7, 9, 0 };
+		int aranacak = 10;
+		boolean varMi = false;
+		for (int sayi : sayilar) {
+			if (sayi == aranacak) {
+				varMi= true ;
+				break;
+			}
+		}
+		if (varMi) {
+			System.out.println("dizide var");
+		} else {
+			System.out.println("dizide bulunmuyor.");
+		}
+
+	}
+
+}
